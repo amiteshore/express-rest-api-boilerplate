cf79649eeaf9691ca363985bc67cece6e8eb21c7
Amit Eshore <amiteshore@gmail.com>
2021-12-22 15:30:54 +0530

    change User model to Author

--- src/config/init_mysql.js
+++ src/config/init_mysql.js
@@ -1,10 +1,10 @@
 import db from "./mysql.js";
-//import User from "../models/User.js";
+//import Author from "../models/Author.js";
 
 (async () => {
   try {
     await db.authenticate();
-    //await User.sync({ alter: true });
+    //await Author.sync({ alter: true });
     console.log("database connected");
   } catch (error) {
     console.log(error);
--- src/models/Author.js
+++ src/models/Author.js
@@ -2,20 +2,15 @@ import { DataTypes } from "sequelize";
 import db from "../config/mysql.js";
 
 export default db.define(
-  "User",
+  "Author",
   {
     name: {
       type: DataTypes.STRING,
       allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
     }
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+    updatedAt: false
+  }
 );
