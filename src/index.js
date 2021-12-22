7e33d6d1f88f6bd3212f50bc13868b5ca657de5b
Amit Eshore <amiteshore@gmail.com>
2021-12-22 14:05:03 +0530

    basic server setup

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import createError from "http-errors";
+import morgan from "morgan";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.set("json spaces", 2);
+
+app.use((req, res, next) => {
+  next(createError.NotFound());
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ message: err.message });
+});
+
+app.listen(PORT, () => {
+  console.log("server running");
+});
