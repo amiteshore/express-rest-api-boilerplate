7b82d3c8beb308543f867037d458925d2a5c6b4b
Amit Eshore <amiteshore@gmail.com>
2022-01-08 21:07:14 +0530

    Cleanup

--- src/index.js
+++ src/index.js
@@ -4,30 +4,33 @@ import morgan from "morgan";
 import dotenv from "dotenv";
 
 import authorsRoute from "./routes/authors.js";
-import "./config/init_mysql.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const HOST = process.env.HOST;
 
-app.use(express.json());
-app.use(morgan("dev"));
 app.set("json spaces", 2);
 
+// Middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+// API routes
 app.use("/authors", authorsRoute);
 
-// handle routes that doesn't exist
+// 404
 app.use((req, res, next) => {
   next(createError.NotFound());
 });
 
-// custom error handler
+// Error handler
 app.use((err, req, res, next) => {
   res.status(err.status || 500);
   res.json({ message: err.message });
 });
 
 app.listen(PORT, () => {
-  console.log("server running");
+  console.log(`started server on http://${HOST}:${PORT}`);
 });
--- src/routes/authors.js
+++ src/routes/authors.js
@@ -1,27 +1,29 @@
-import express from "express";
-import {
-  createAuthor,
-  getAuthors,
-  getAuthor,
-  editAuthor,
-  deleteAuthor
-} from "../controllers/authors.js";
+import { Router } from "express";
+import { NotFound } from "http-errors";
 
-const router = express.Router();
+const router = Router();
 
-// create an author
-router.post("/", createAuthor);
+const authors = [
+  { id: 1, name: "Brandon Sanderson" },
+  { id: 2, name: "Robert Jordan" },
+  { id: 3, name: "Patrick Rothfuss" }
+];
 
 // get all authors
-router.get("/", getAuthors);
+router.get("/", (req, res) => {
+  res.status(200).json(authors);
+});
 
-// get an author
-router.get("/:id", getAuthor);
-
-// edit an author
-router.put("/:id", editAuthor);
-
-// delete an author
-router.delete("/:id", deleteAuthor);
+// get single author
+router.get("/:id", (req, res, next) => {
+  try {
+    const id = parseFloat(req.params.id);
+    const author = authors.find((a) => a.id === id);
+    if (!author) throw NotFound();
+    res.status(200).json(author);
+  } catch (error) {
+    next(error);
+  }
+});
 
 export default router;
